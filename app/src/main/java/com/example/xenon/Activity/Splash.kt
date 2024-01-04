a8e6b3f5fa57df51bc5a2f92844ca1385f9f3b77
Ishant Kumawat <120731858+insanecoder02@users.noreply.github.com>
2024-01-04 17:21:53 +0530

    commit 2 ishant

---
+++ app/src/main/java/com/example/xenon/Activity/Splash.kt
@@ -0,0 +1,22 @@
+package com.example.xenon.Activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.xenon.databinding.ActivitySplashBinding
+
+class Splash : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, GetStarted::class.java))
+        },2000)
+//        finish()
+    }
+}
