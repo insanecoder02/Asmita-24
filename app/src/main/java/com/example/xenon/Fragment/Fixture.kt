a229c9eb276dab62434c08d1a39b6d87727ff56a
Ishant Kumawat <120731858+insanecoder02@users.noreply.github.com>
2024-02-24 19:44:30 +0530

    commit 27 ishant

--- app/src/main/java/com/example/xenon/Fragment/Fixture.kt
+++ app/src/main/java/com/example/xenon/Fragment/Fixture.kt
@@ -8,10 +8,9 @@ import android.view.ViewGroup
 import com.example.xenon.databinding.FragmentFixtureBinding
 
 class Fixture : Fragment() {
-    private lateinit var binding:FragmentFixtureBinding
+    private lateinit var binding: FragmentFixtureBinding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentFixtureBinding.inflate(layoutInflater, container, false)
         return binding.root
@@ -22,39 +21,7 @@ class Fixture : Fragment() {
         val name = arguments?.getString("name")
         binding.text1.text = name
         val htmlContent = """
-            <p>&nbsp;</p>
-            <table style="border-collapse: collapse; width: 100%; color: white;" border="1">
-                <colgroup>
-                    <col style="width: 33.33%;">
-                    <col style="width: 33.33%;">
-                    <col style="width: 33.33%;">
-                </colgroup>
-                <tbody>
-                    <tr>
-                        <td colspan="3" style="text-align: center;"><strong>Match Details</strong></td>
-                    </tr>
-                    <tr>
-                        <td><strong>Team A</strong></td>
-                        <td><strong>Match Date</strong></td>
-                        <td><strong>Match Time</strong></td>
-                    </tr>
-                    <tr>
-                        <td>IOIIT</td>
-                        <td>2024-02-20</td>
-                        <td>10:00 AM</td>
-                    </tr>
-                    <tr>
-                        <td>XYZ College</td>
-                        <td>2024-02-21</td>
-                        <td>3:30 PM</td>
-                    </tr>
-                    <tr>
-                        <td>ABC University</td>
-                        <td>2024-02-22</td>
-                        <td>6:00 PM</td>
-                    </tr>
-                </tbody>
-            </table>
+        <table border ='1' cellpadding = '1' cellspacing = '1' style = 'width:500px'><tbody><tr><td style = 'text-align:center'><span style = 'color:#e74c3c'><strong>Team A</strong></span></td><td style = 'text-align:center'><span style = 'color:#e74c3c'><strong>Team B</strong></span></td><td style = 'text-align:center'><span style = 'color:#e74c3'><strong>Time</strong></span></td></tr><tr><td style = 'text-align:center'>IIITA</td><td style = 'text-align:center'>IIITB</td><td style = 'text-align:center'>09/03/2024, 3pm</td></tr><tr><td style = 'text-align:center'>IIITC</td><td style = 'text-align:center'>IIITD</td><td style = 'text-align:center'>09/03/2024, 5pm</td></tr><tr><td style = 'text-align:center'>IIITE</td><td style = 'text-align:center'>IIITF</td><td style = 'text-align:center'>10/03/2024, 9pm</td></tr><tr><td style = 'text-align:center'>IIITG</td><td style = 'text-align:center'>IIITH</td><td style = 'text-align:center'>09/03/2024, 10pm</td></tr></tbody</table><p>&nbsp;</p>
         """.trimIndent()
 
         binding.web.loadDataWithBaseURL(null, htmlContent, "text/html", "UTF-8", null)
