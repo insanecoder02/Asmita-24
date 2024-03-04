5a01169ce7aa57421f598d925d305a6a280280a6
Naitik <iib2023036@iiita.ac.in>
2024-03-05 02:07:31 +0530

    commit naitik developer

--- app/src/main/java/com/interiiit/xenon/Adapter/DevAdapter.kt
+++ app/src/main/java/com/interiiit/xenon/Adapter/DevAdapter.kt
@@ -1,10 +1,15 @@
 package com.interiiit.xenon.Adapter
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -12,20 +17,23 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.interiiit.xenon.DataClass.DeveloperDataClass
 import com.interiiit.xenon.R
 
-class DevAdapter(private val dev:MutableList<DeveloperDataClass>):RecyclerView.Adapter<DevAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
-        val name:TextView = itemView.findViewById(R.id.nameTextView)
-        val desig:TextView = itemView.findViewById(R.id.desginations)
-        val img:ImageView = itemView.findViewById(R.id.imageView)
+class DevAdapter(private val dev: MutableList<DeveloperDataClass>) :
+    RecyclerView.Adapter<DevAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.nameTextView)
+        val desig: TextView = itemView.findViewById(R.id.desginations)
+        val img: ImageView = itemView.findViewById(R.id.imageView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_developer , parent , false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_developer, parent, false)
         return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        return  dev.size
+        return dev.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -41,5 +49,15 @@ class DevAdapter(private val dev:MutableList<DeveloperDataClass>):RecyclerView.A
             .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
             .into(holder.img)
 
+        if(devel.link != ""){
+            holder.itemView.setOnClickListener {
+                val openUrl = Intent(Intent.ACTION_VIEW)
+                openUrl.data = Uri.parse(devel.link)
+                holder.itemView.context.startActivity(openUrl)
+            }
+        }
+        else{
+            Toast.makeText(holder.itemView.context, "Link Not Found", Toast.LENGTH_SHORT).show()
+        }
     }
 }
